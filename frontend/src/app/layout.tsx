13c768d0e67cb76ab4869e9874480beb33a0cbf5
Ritesh <github.riteshkumar@gmail.com>
2025-07-15 09:50:12 +0530

    Intialised

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Manrope } from "next/font/google";
+import "./globals.css";
+
+const manrope = Manrope({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Behind It",
+  description: "See full convolutional analysis of audios.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" type="image/svg+xml" href="/logo.svg" />
+      </head>
+      <body
+        className={`${manrope.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
